d72af5566114707089418f1d672653c062e16a50
hangyeol0417 <63591036+hangyeol0417@users.noreply.github.com>
2021-05-29 00:29:17 +0900

    [2021.05.29]

---
+++ src/controllers/videoController.js
@@ -0,0 +1,15 @@
+export const trending = (req, res) => res.send("Home Page Videos");
+export const see = (req, res) => {
+    console.log(req.params.id);
+    res.send("See");
+}
+export const edit = (req, res) => {
+    console.log(req.params.id);
+    res.send("Edit");
+}
+export const search = (req, res) => res.send("Search");
+export const upload = (req, res) => res.send("Upload");
+export const deleteVideo = (req, res) => {
+    console.log(req.params.id);
+    res.send("Delete Video");
+}
--- src/server.js
+++ src/server.js
@@ -1,23 +1,21 @@
 import express from "express";
 import morgan from "morgan";
+import globalRouter from "./routers/globalRouter"
+import videoRouter from "./routers/videoRouter"
+import userRouter from "./routers/userRouter"
 
 const PORT = 4000;
 
 const app = express();
 const logger = morgan("dev");
-
-const home = (req, res) => {
-    console.log("home")
-    return res.end("hello");
-};
-
-const login = (req, res, next) => {
-    return res.end("login");
-};
-
 app.use(logger)
-app.get("/", home);
-app.get("/login", login);
+
+
+
+app.use("/", globalRouter)
+app.use("/videos", videoRouter)
+app.use("/users", userRouter)
+
 
 const handleListening = () =>
     console.log(`Server listenting on port http://localhost:${PORT} 🚀`)
