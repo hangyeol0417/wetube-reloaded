317a4184b94c47accddea79e4f669c1bb88fc538
hangyeol0417 <63591036+hangyeol0417@users.noreply.github.com>
2021-06-17 23:57:03 +0900

    [2021.06.17]

--- src/models/Video.js
+++ src/models/Video.js
@@ -8,13 +8,13 @@ const videoSchema = new mongoose.Schema({
     hashtags: [{ type: String, trim: true }],
     meta: {
         views: { type: Number, default: 0, required: true },
-        rating: { type: Number, default: 0, required: true },
     },
+    owner: { type: mongoose.Schema.Types.ObjectId, required: true, ref: "User" },
 });
 
 videoSchema.static('formatHashtags', function(hashtags) {
     return hashtags.split(",").map((word) => (word.startsWith(`#`) ? word : `#${word}`))
 })
 
-const Video = mongoose.model("video", videoSchema);
+const Video = mongoose.model("Video", videoSchema);
 export default Video;
