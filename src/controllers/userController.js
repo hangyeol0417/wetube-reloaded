5b0a61cef6ea533ea52551e1758b520441999005
hangyeol0417 <63591036+hangyeol0417@users.noreply.github.com>
2021-07-05 03:05:31 +0900

    s3 uploads

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -162,7 +162,7 @@ export const PostEdit = async(req, res) => {
     }
     try {
         const updatedUser = await User.findByIdAndUpdate(_id, {
-            avatarUrl: file ? file.path : avatarUrl,
+            avatarUrl: file ? file.location : avatarUrl,
             name,
             email,
             username,
